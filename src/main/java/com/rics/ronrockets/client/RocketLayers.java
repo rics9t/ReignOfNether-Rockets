af1c16e46269ffdb991c37ffde7d85750a75ff9e
rics9t <andreygamer190@gmail.com>
2026-04-26 21:32:09 +0500

    Add RocketLayers class with ROCKET_LAYER constant

---
+++ src/main/java/com/rics/ronrockets/client/RocketLayers.java
@@ -0,0 +1,13 @@
+package com.rics.ronrockets.client;
+
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.resources.ResourceLocation;
+
+public class RocketLayers {
+
+    public static final ModelLayerLocation ROCKET_LAYER =
+            new ModelLayerLocation(
+                    new ResourceLocation("ronrockets", "rocket"),
+                    "main"
+            );
+}
