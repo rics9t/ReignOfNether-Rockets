780de4516447c7bd8e1dfb9b1e0889cea5d4ef2c
rics9t <andreygamer190@gmail.com>
2026-04-26 21:20:11 +0500

    Add ClientSetup for entity renderer registration

---
+++ src/main/java/com/rics/ronrockets/client/ClientSetup.java
@@ -0,0 +1,16 @@
+package com.rics.ronrockets.client;
+
+import com.rics.ronrockets.entity.RocketEntities;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientSetup {
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(RocketEntities.ROCKET.get(), RocketRenderer::new);
+    }
+}
