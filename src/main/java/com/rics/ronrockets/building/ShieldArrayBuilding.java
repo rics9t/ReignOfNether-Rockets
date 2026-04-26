3ccebf245a3ce41122982e20bf35b44cd45af54a
rics9t <andreygamer190@gmail.com>
2026-04-26 20:23:37 +0500

    Refactor ShieldArrayBuilding constructor and methods
    Refactored ShieldArrayBuilding class to include abilities initialization in the constructor and added getBuildButton method.

--- src/main/java/com/rics/ronrockets/building/ShieldArrayBuilding.java
+++ src/main/java/com/rics/ronrockets/building/ShieldArrayBuilding.java
@@ -1,29 +1,50 @@
 package com.rics.ronrockets.building;
 
+import com.rics.ronrockets.ability.ShieldInterceptAbility;
 import com.solegendary.reignofnether.building.Building;
+import com.solegendary.reignofnether.building.BuildingClientEvents;
+import com.solegendary.reignofnether.building.BuildingPlaceButton;
 import com.solegendary.reignofnether.faction.Faction;
+import com.solegendary.reignofnether.keybinds.Keybinding;
 import com.solegendary.reignofnether.resources.ResourceCost;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.List;
 
 public class ShieldArrayBuilding extends Building {
 
     public static final int SHIELD_RADIUS = 64;
 
-    this.abilities.add(new ShieldInterceptAbility());
-
-    public ShieldArrayBuilding(String structureName, Faction faction) {
+    public ShieldArrayBuilding(String structureName) {
         super(structureName,
                 new ResourceCost(200, 300, 500, 0, 0),
                 false
         );
 
         this.name = "Shield Array";
-        this.portraitBlock = net.minecraft.world.level.block.Blocks.BEACON;
+        this.portraitBlock = Blocks.BEACON;
+
+        // ✅ Ability must be added INSIDE constructor
+        this.abilities.add(new ShieldInterceptAbility());
     }
-    
-    public abstract BuildingPlaceButton getBuildButton(Keybinding hotkey);
-    
+
     @Override
     public Faction getFaction() {
         return Faction.NONE;
+    }
+
+    @Override
+    public BuildingPlaceButton getBuildButton(Keybinding hotkey) {
+        return new BuildingPlaceButton(
+                this.name,
+                ResourceLocation.withDefaultNamespace("textures/block/beacon.png"),
+                hotkey,
+                () -> BuildingClientEvents.getBuildingToPlace() == this,
+                () -> false,
+                () -> true,
+                List.of(),
+                this
+        );
     }
 }
