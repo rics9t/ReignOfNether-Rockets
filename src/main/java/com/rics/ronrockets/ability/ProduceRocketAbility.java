a85da0ce263cb147a0f99d5107c05cf9eea6e853
rics9t <andreygamer190@gmail.com>
2026-04-26 20:33:21 +0500

    Update ProduceRocketAbility.java

--- src/main/java/com/rics/ronrockets/ability/ProduceRocketAbility.java
+++ src/main/java/com/rics/ronrockets/ability/ProduceRocketAbility.java
@@ -1,9 +1,18 @@
 package com.rics.ronrockets.ability;
 
+import com.rics.ronrockets.RonRocketsMod;
 import com.solegendary.reignofnether.ability.Ability;
 import com.solegendary.reignofnether.building.BuildingPlacement;
+import com.solegendary.reignofnether.hud.AbilityButton;
+import com.solegendary.reignofnether.keybinds.Keybinding;
 import com.solegendary.reignofnether.unit.UnitAction;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class ProduceRocketAbility extends Ability {
 
@@ -15,14 +24,45 @@ public class ProduceRocketAbility extends Ability {
     }
 
     @Override
+    public AbilityButton getButton(Keybinding hotkey, BuildingPlacement placement) {
+
+        int charges = placement.getCharges(this);
+
+        return new AbilityButton(
+                "Produce Rocket",
+                new ResourceLocation(RonRocketsMod.MODID, "textures/icons/produce_rocket.png"),
+                hotkey,
+                () -> false,
+                () -> charges >= maxCharges,
+                () -> charges < maxCharges,
+                () -> this.use(placement.getLevel(), placement, placement.centrePos),
+                null,
+                List.of(
+                        FormattedCharSequence.forward(
+                                I18n.get("abilities.ronrockets.produce_rocket"),
+                                Style.EMPTY.withBold(true)
+                        ),
+                        FormattedCharSequence.forward(
+                                "Production Time: 120s",
+                                Style.EMPTY
+                        ),
+                        FormattedCharSequence.forward(
+                                "Stored Rockets: " + charges + "/2",
+                                Style.EMPTY
+                        )
+                ),
+                this,
+                placement
+        );
+    }
+
+    @Override
     public void use(Level level, BuildingPlacement buildingUsing, net.minecraft.core.BlockPos bp) {
 
         if (level.isClientSide()) return;
 
-        // Do not produce if already full
         if (buildingUsing.getCharges(this) >= maxCharges) return;
 
-        // Start cooldown
         buildingUsing.setCooldown(this, cooldownMax);
     }
 }
