d67ca202ab2e0385dd8bab2c666d8d9f8029e704
rics9t <andreygamer190@gmail.com>
2026-04-26 20:45:24 +0500

    Add RocketClientEvents to handle incoming rockets

---
+++ src/main/java/com/rics/ronrockets/client/RocketClientEvents.java
@@ -0,0 +1,41 @@
+package com.rics.ronrockets.client;
+
+import com.rics.ronrockets.rocket.RocketStrike;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RocketClientEvents {
+
+    private static final Minecraft MC = Minecraft.getInstance();
+    private static final List<BlockPos> INCOMING_ROCKETS = new ArrayList<>();
+
+    public static void addIncomingRocket(BlockPos pos) {
+        INCOMING_ROCKETS.add(pos);
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+
+        if (event.phase != TickEvent.Phase.END) return;
+        if (MC.level == null) return;
+
+        for (BlockPos pos : INCOMING_ROCKETS) {
+
+            MC.level.addParticle(
+                    ParticleTypes.SMOKE,
+                    pos.getX() + 0.5,
+                    pos.getY() + 5,
+                    pos.getZ() + 0.5,
+                    0,
+                    -0.2,
+                    0
+            );
+        }
+    }
+}
