712059dbdd89be8b59784d887d914068b0419117
rics9t <andreygamer190@gmail.com>
2026-04-28 01:27:00 +0500

    Update ProduceRocketAbility.java

--- src/main/java/com/rics/ronrockets/ability/ProduceRocketAbility.java
+++ src/main/java/com/rics/ronrockets/ability/ProduceRocketAbility.java
@@ -7,11 +7,6 @@ public class ProduceRocketAbility extends Ability {
 
     public static final int MAX_ROCKETS = 2;
 
-    /**
-     * Используется как ключ для системы зарядов BuildingPlacement.
-     * Каждое здание хранит свой счётчик зарядов по этому ключу.
-     * Синглтон безопасен, т.к. charges хранятся в BuildingPlacement, а не здесь.
-     */
     public static final ProduceRocketAbility INSTANCE = new ProduceRocketAbility();
 
     private ProduceRocketAbility() {
