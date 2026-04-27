00e433e19a8298e46722e4c83060d8b8cef2941d
rics9t <andreygamer190@gmail.com>
2026-04-27 08:47:07 +0500

    Refactor PiglinRocketSilo to extend Building class

--- src/main/java/com/rics/ronrockets/building/PiglinRocketSilo.java
+++ src/main/java/com/rics/ronrockets/building/PiglinRocketSilo.java
@@ -1,17 +1,35 @@
 package com.rics.ronrockets.building;
 
+import com.solegendary.reignofnether.api.ReignOfNetherRegistries;
 import com.solegendary.reignofnether.building.BuildingClientEvents;
 import com.solegendary.reignofnether.building.BuildingPlaceButton;
-import com.solegendary.reignofnether.faction.Faction;
 import com.solegendary.reignofnether.keybinds.Keybinding;
+import com.solegendary.reignofnether.keybinds.Keybindings;
+import com.solegendary.reignofnether.faction.Faction;
+import com.solegendary.reignofnether.resources.ResourceCost;
+import com.solegendary.reignofnether.resources.ResourceCosts;
+
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 
-public class PiglinRocketSilo extends AbstractRocketSilo {
+import java.util.List;
+
+import static com.solegendary.reignofnether.util.MiscUtil.fcs;
+
+public class PiglinRocketSilo extends com.solegendary.reignofnether.building.Building {
+
+    public static final String STRUCTURE_NAME = "piglin_rocket_silo";
+    public static final ResourceCost COST = ResourceCost.Building(1000, 800, 600, 0);
 
     public PiglinRocketSilo() {
-        super("piglin_rocket_silo");
+        super(STRUCTURE_NAME, COST, false);
+
+        this.name = "Rocket Silo";
         this.portraitBlock = Blocks.IRON_BLOCK;
+        this.icon = ResourceLocation.fromNamespaceAndPath(
+                "minecraft", "textures/block/iron_block.png"
+        );
     }
 
     @Override
@@ -21,14 +39,29 @@ public class PiglinRocketSilo extends AbstractRocketSilo {
 
     @Override
     public BuildingPlaceButton getBuildButton(Keybinding hotkey) {
+
+        ResourceLocation key = ReignOfNetherRegistries.BUILDING.getKey(this);
+
+        String name = I18n.get(
+                "buildings." +
+                        getFaction().name().toLowerCase() +
+                        "." +
+                        key.getNamespace() +
+                        "." +
+                        key.getPath()
+        );
+
         return new BuildingPlaceButton(
-                this.name,
-                ResourceLocation.withDefaultNamespace("textures/block/iron_block.png"),
+                name,
+                this.icon,
                 hotkey,
                 () -> BuildingClientEvents.getBuildingToPlace() == this,
                 () -> false,
                 () -> true,
-                java.util.List.of(),
+                List.of(
+                        fcs(name, true),
+                        ResourceCosts.getFormattedCost(COST)
+                ),
                 this
         );
     }
