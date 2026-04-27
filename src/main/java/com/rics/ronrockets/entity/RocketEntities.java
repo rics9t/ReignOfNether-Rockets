3358772ced8f1a35f0d1ddb89bc73f4305e6e4d4
rics9t <andreygamer190@gmail.com>
2026-04-28 01:12:39 +0500

    start

--- src/main/java/com/rics/ronrockets/entity/RocketEntities.java
+++ src/main/java/com/rics/ronrockets/entity/RocketEntities.java
@@ -1,8 +1,10 @@
 package com.rics.ronrockets.entity;
 
 import com.rics.ronrockets.RonRocketsMod;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,14 +15,15 @@ public class RocketEntities {
             DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, RonRocketsMod.MODID);
 
     public static final RegistryObject<EntityType<RocketEntity>> ROCKET =
-            ENTITIES.register("rocket",
-                    () -> EntityType.Builder.<RocketEntity>of(RocketEntity::new, MobCategory.MISC)
-                            .sized(0.5f, 1.5f)
-                            .clientTrackingRange(128)
-                            .updateInterval(1)
-                            .build("rocket"));
+            ENTITIES.register("rocket", () ->
+                    EntityType.Builder.<RocketEntity>of(RocketEntity::new, MobCategory.MISC)
+                            .sized(0.5F, 1.5F)           
+                            .clientTrackingRange(128)    
+                            .updateInterval(1)           
+                            .build(new ResourceLocation(RonRocketsMod.MODID, "rocket").toString())
+            );
 
-    public static void register(net.minecraftforge.eventbus.api.IEventBus bus) {
-        ENTITIES.register(bus);
+    public static void register(IEventBus modBus) {
+        ENTITIES.register(modBus);
     }
 }
