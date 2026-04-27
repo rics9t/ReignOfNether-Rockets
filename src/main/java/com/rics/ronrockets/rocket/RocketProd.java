3dd175f5793a24b07a7fff9fc75103d83524ac74
rics9t <andreygamer190@gmail.com>
2026-04-27 23:06:13 +0500

    Refactor ability handling in RocketProd class

--- src/main/java/com/rics/ronrockets/rocket/RocketProd.java
+++ src/main/java/com/rics/ronrockets/rocket/RocketProd.java
@@ -1,7 +1,6 @@
 package com.rics.ronrockets.rocket;
 
 import com.rics.ronrockets.ability.ProduceRocketAbility;
-import com.solegendary.reignofnether.building.BuildingPlacement;
 import com.solegendary.reignofnether.building.buildings.placements.ProductionPlacement;
 import com.solegendary.reignofnether.building.production.ProductionItem;
 import com.solegendary.reignofnether.building.production.StartProductionButton;
@@ -26,15 +25,14 @@ public class RocketProd extends ProductionItem {
 
         this.onComplete = (Level level, ProductionPlacement placement) -> {
             if (!level.isClientSide()) {
-                for (var ability : placement.getAbilities()) {
+                placement.getAbilities().forEach(ability -> {
                     if (ability instanceof ProduceRocketAbility produce) {
                         int current = placement.getCharges(produce);
                         if (current < produce.maxCharges) {
                             placement.setCharges(produce, current + 1);
                         }
-                        break;
                     }
-                }
+                });
             }
         };
     }
@@ -52,14 +50,9 @@ public class RocketProd extends ProductionItem {
                 ResourceLocation.fromNamespaceAndPath("ronrockets", "textures/icons/produce_rocket.png"),
                 hotkey,
                 () -> false,
-                () -> {
-                    for (var ability : placement.getAbilities()) {
-                        if (ability instanceof ProduceRocketAbility produce) {
-                            return placement.getCharges(produce) >= produce.maxCharges;
-                        }
-                    }
-                    return false;
-                },
+                () -> placement.getAbilities().stream()
+                        .anyMatch(a -> a instanceof ProduceRocketAbility produce &&
+                                placement.getCharges(produce) >= produce.maxCharges),
                 List.of(
                         FormattedCharSequence.forward("Produce Rocket", Style.EMPTY.withBold(true)),
                         ResourceCosts.getFormattedCost(COST),
