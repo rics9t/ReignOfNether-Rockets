febcb425661023ceb54aa036515e6fe4b13aeae7
rics9t <andreygamer190@gmail.com>
2026-04-27 10:22:33 +0500

    Refactor PiglinRocketSilo with new features
    Updated the PiglinRocketSilo class to include new functionalities and changed the portrait block to Gilded Blackstone.

--- src/main/java/com/rics/ronrockets/building/PiglinRocketSilo.java
+++ src/main/java/com/rics/ronrockets/building/PiglinRocketSilo.java
@@ -1,35 +1,43 @@
 package com.rics.ronrockets.building;
 
 import com.solegendary.reignofnether.api.ReignOfNetherRegistries;
-import com.solegendary.reignofnether.building.BuildingClientEvents;
-import com.solegendary.reignofnether.building.BuildingPlaceButton;
-import com.solegendary.reignofnether.keybinds.Keybinding;
-import com.solegendary.reignofnether.keybinds.Keybindings;
+import com.solegendary.reignofnether.building.*;
 import com.solegendary.reignofnether.faction.Faction;
+import com.solegendary.reignofnether.keybinds.Keybinding;
 import com.solegendary.reignofnether.resources.ResourceCost;
 import com.solegendary.reignofnether.resources.ResourceCosts;
 
 import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Rotation;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.solegendary.reignofnether.building.BuildingUtils.getAbsoluteBlockData;
 import static com.solegendary.reignofnether.util.MiscUtil.fcs;
 
-public class PiglinRocketSilo extends com.solegendary.reignofnether.building.Building {
+public class PiglinRocketSilo extends Building {
 
     public static final String STRUCTURE_NAME = "piglin_rocket_silo";
-    public static final ResourceCost COST = ResourceCost.Building(1000, 800, 600, 0);
+    public static final ResourceCost COST =
+            ResourceCost.Building(1000, 800, 600, 0);
 
     public PiglinRocketSilo() {
         super(STRUCTURE_NAME, COST, false);
 
         this.name = "Rocket Silo";
-        this.portraitBlock = Blocks.IRON_BLOCK;
+        this.portraitBlock = Blocks.GILDED_BLACKSTONE;
         this.icon = ResourceLocation.fromNamespaceAndPath(
-                "minecraft", "textures/block/iron_block.png"
+                "minecraft",
+                "textures/block/gilded_blackstone.png"
         );
+
+        this.startingBlockTypes.add(Blocks.GILDED_BLACKSTONE);
     }
 
     @Override
@@ -38,14 +46,44 @@ public class PiglinRocketSilo extends com.solegendary.reignofnether.building.Bui
     }
 
     @Override
+    public ArrayList<BuildingBlock> getRelativeBlockData(LevelAccessor level) {
+        return BuildingBlockData.getBuildingBlocksFromNbt(
+                this.structureName,
+                level
+        );
+    }
+
+    @Override
+    public BuildingPlacement createBuildingPlacement(
+            Level level,
+            BlockPos pos,
+            Rotation rotation,
+            String ownerName
+    ) {
+        return new BuildingPlacement(
+                this,
+                level,
+                pos,
+                rotation,
+                ownerName,
+                getAbsoluteBlockData(
+                        getRelativeBlockData(level),
+                        level,
+                        pos,
+                        rotation
+                ),
+                false
+        );
+    }
+
+    @Override
     public BuildingPlaceButton getBuildButton(Keybinding hotkey) {
 
-        ResourceLocation key = ReignOfNetherRegistries.BUILDING.getKey(this);
+        ResourceLocation key =
+                ReignOfNetherRegistries.BUILDING.getKey(this);
 
         String name = I18n.get(
-                "buildings." +
-                        getFaction().name().toLowerCase() +
-                        "." +
+                "buildings.piglins." +
                         key.getNamespace() +
                         "." +
                         key.getPath()
@@ -55,7 +93,8 @@ public class PiglinRocketSilo extends com.solegendary.reignofnether.building.Bui
                 name,
                 this.icon,
                 hotkey,
-                () -> BuildingClientEvents.getBuildingToPlace() == RocketBuildings.PIGLIN_SILO,
+                () -> BuildingClientEvents.getBuildingToPlace()
+                        == RocketBuildings.PIGLIN_SILO,
                 () -> false,
                 () -> true,
                 List.of(
