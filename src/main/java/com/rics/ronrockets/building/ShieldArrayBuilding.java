38d6ea10e5cb7fa3f43cc893ddc68803e6a385ec
rics9t <andreygamer190@gmail.com>
2026-04-27 08:52:18 +0500

    Update ShieldArrayBuilding.java

--- src/main/java/com/rics/ronrockets/building/ShieldArrayBuilding.java
+++ src/main/java/com/rics/ronrockets/building/ShieldArrayBuilding.java
@@ -1,49 +1,73 @@
 package com.rics.ronrockets.building;
 
-import com.rics.ronrockets.ability.ShieldInterceptAbility;
+import com.solegendary.reignofnether.api.ReignOfNetherRegistries;
 import com.solegendary.reignofnether.building.Building;
 import com.solegendary.reignofnether.building.BuildingClientEvents;
 import com.solegendary.reignofnether.building.BuildingPlaceButton;
 import com.solegendary.reignofnether.faction.Faction;
 import com.solegendary.reignofnether.keybinds.Keybinding;
 import com.solegendary.reignofnether.resources.ResourceCost;
+import com.solegendary.reignofnether.resources.ResourceCosts;
+
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 
 import java.util.List;
 
+import static com.solegendary.reignofnether.util.MiscUtil.fcs;
+
 public class ShieldArrayBuilding extends Building {
+
+    public static final String STRUCTURE_NAME = "shield_array";
+    public static final ResourceCost COST = ResourceCost.Building(200, 300, 500, 0);
 
     public static final int SHIELD_RADIUS = 64;
 
-    public ShieldArrayBuilding(String structureName) {
-        super(structureName,
-                ResourceCost.Building(200, 300, 500, 0),
-                false
-        );
+    public ShieldArrayBuilding() {
+        super(STRUCTURE_NAME, COST, false);
 
         this.name = "Shield Array";
         this.portraitBlock = Blocks.BEACON;
-
-        // ✅ Ability must be added INSIDE constructor
-        this.abilities.add(new ShieldInterceptAbility());
+        this.icon = ResourceLocation.fromNamespaceAndPath(
+                "minecraft",
+                "textures/block/beacon.png"
+        );
     }
 
     @Override
     public Faction getFaction() {
-        return Faction.NONE;
+        return Faction.NONE; // Will be registered to factions manually
     }
 
     @Override
     public BuildingPlaceButton getBuildButton(Keybinding hotkey) {
+
+        ResourceLocation key = ReignOfNetherRegistries.BUILDING.getKey(this);
+
+        String name = I18n.get(
+                "buildings." +
+                        "ronrockets" + "." +
+                        key.getNamespace() +
+                        "." +
+                        key.getPath()
+        );
+
         return new BuildingPlaceButton(
-                this.name,
-                ResourceLocation.withDefaultNamespace("textures/block/beacon.png"),
+                name,
+                this.icon,
                 hotkey,
                 () -> BuildingClientEvents.getBuildingToPlace() == this,
                 () -> false,
                 () -> true,
-                List.of(),
+                List.of(
+                        fcs(name, true),
+                        ResourceCosts.getFormattedCost(COST),
+                        fcs(""),
+                        fcs("Intercept Radius: 64"),
+                        fcs("Activation Cost: 150 Ore"),
+                        fcs("Cooldown: 30s")
+                ),
                 this
         );
     }
