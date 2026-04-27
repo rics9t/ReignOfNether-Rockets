a85ed17b2fdbd29c903ded12757b7f305cdb66cb
rics9t <andreygamer190@gmail.com>
2026-04-27 07:58:17 +0500

    Refactor RocketModel class and update imports

--- src/main/java/com/rics/ronrockets/client/model/RocketModel.java
+++ src/main/java/com/rics/ronrockets/client/model/RocketModel.java
@@ -3,54 +3,140 @@ package com.rics.ronrockets.client.model;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.rics.ronrockets.entity.RocketEntity;
+
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 
-// Made with Blockbench 5.1.4
-// Exported for Minecraft version 1.17 or later with Mojang mappings
-// Paste this class into your mod and generate all required imports
+public class RocketModel extends EntityModel<RocketEntity> {
 
+    public static final ModelLayerLocation LAYER_LOCATION =
+            new ModelLayerLocation(
+                    ResourceLocation.fromNamespaceAndPath("ronrockets", "rocket"),
+                    "main"
+            );
 
-public class RocketModel<T extends RocketEntity> extends EntityModel<T> {
-	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "rocket"), "main");
-	private final ModelPart bb_main;
+    private final ModelPart bb_main;
 
-	public RocketModel(ModelPart root) {
-		this.bb_main = root.getChild("bb_main");
-	}
+    public RocketModel(ModelPart root) {
+        this.bb_main = root.getChild("bb_main");
+    }
 
-	public static LayerDefinition createBodyLayer() {
-		MeshDefinition meshdefinition = new MeshDefinition();
-		PartDefinition partdefinition = meshdefinition.getRoot();
+    public static LayerDefinition createBodyLayer() {
 
-		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -37.0F, -4.0F, 8.0F, 32.0F, 8.0F, new CubeDeformation(0.0F))
-		.texOffs(32, 38).addBox(-3.0F, -5.0F, -3.0F, 6.0F, 2.0F, 6.0F, new CubeDeformation(0.0F))
-		.texOffs(32, 22).addBox(-3.0F, -47.0F, -3.0F, 6.0F, 10.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition cube_r1 = bb_main.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(32, 11).addBox(-21.0F, -2.0F, -1.0F, 22.0F, 9.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-3.0F, -31.0F, 1.0F, 0.0F, -0.0873F, -0.1745F));
+        PartDefinition bb_main =
+                partdefinition.addOrReplaceChild(
+                        "bb_main",
+                        CubeListBuilder.create()
+                                .texOffs(0, 0)
+                                .addBox(-4.0F, -37.0F, -4.0F,
+                                        8.0F, 32.0F, 8.0F,
+                                        new CubeDeformation(0.0F))
+                                .texOffs(32, 38)
+                                .addBox(-3.0F, -5.0F, -3.0F,
+                                        6.0F, 2.0F, 6.0F,
+                                        new CubeDeformation(0.0F))
+                                .texOffs(32, 22)
+                                .addBox(-3.0F, -47.0F, -3.0F,
+                                        6.0F, 10.0F, 6.0F,
+                                        new CubeDeformation(0.0F)),
+                        PartPose.offset(0.0F, 24.0F, 0.0F)
+                );
 
-		PartDefinition cube_r2 = bb_main.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(32, 0).addBox(-1.0F, -2.0F, -1.0F, 22.0F, 9.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, -31.0F, 1.0F, 0.0F, 0.0873F, 0.1745F));
+        bb_main.addOrReplaceChild(
+                "cube_r1",
+                CubeListBuilder.create()
+                        .texOffs(32, 11)
+                        .addBox(-21.0F, -2.0F, -1.0F,
+                                22.0F, 9.0F, 2.0F,
+                                new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(
+                        -3.0F, -31.0F, 1.0F,
+                        0.0F, -0.0873F, -0.1745F)
+        );
 
-		PartDefinition cube_r3 = bb_main.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(40, 46).addBox(-5.0F, -6.0F, -1.0F, 8.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -5.0F, 4.0F, -1.5708F, 1.3963F, -1.5708F));
+        bb_main.addOrReplaceChild(
+                "cube_r2",
+                CubeListBuilder.create()
+                        .texOffs(32, 0)
+                        .addBox(-1.0F, -2.0F, -1.0F,
+                                22.0F, 9.0F, 2.0F,
+                                new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(
+                        3.0F, -31.0F, 1.0F,
+                        0.0F, 0.0873F, 0.1745F)
+        );
 
-		PartDefinition cube_r4 = bb_main.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(20, 46).addBox(-5.0F, -6.0F, -1.0F, 8.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.0F, -5.0F, 0.0F, 0.0F, 0.0F, -0.1745F));
+        bb_main.addOrReplaceChild(
+                "cube_r3",
+                CubeListBuilder.create()
+                        .texOffs(40, 46)
+                        .addBox(-5.0F, -6.0F, -1.0F,
+                                8.0F, 6.0F, 2.0F,
+                                new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(
+                        0.0F, -5.0F, 4.0F,
+                        -1.5708F, 1.3963F, -1.5708F)
+        );
 
-		PartDefinition cube_r5 = bb_main.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(0, 40).addBox(-3.0F, -6.0F, -1.0F, 8.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.0F, -5.0F, 0.0F, 0.0F, 0.0F, 0.1745F));
+        bb_main.addOrReplaceChild(
+                "cube_r4",
+                CubeListBuilder.create()
+                        .texOffs(20, 46)
+                        .addBox(-5.0F, -6.0F, -1.0F,
+                                8.0F, 6.0F, 2.0F,
+                                new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(
+                        -4.0F, -5.0F, 0.0F,
+                        0.0F, 0.0F, -0.1745F)
+        );
 
-		return LayerDefinition.create(meshdefinition, 128, 128);
-	}
+        bb_main.addOrReplaceChild(
+                "cube_r5",
+                CubeListBuilder.create()
+                        .texOffs(0, 40)
+                        .addBox(-3.0F, -6.0F, -1.0F,
+                                8.0F, 6.0F, 2.0F,
+                                new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(
+                        4.0F, -5.0F, 0.0F,
+                        0.0F, 0.0F, 0.1745F)
+        );
 
-	@Override
-	public void setupAnim(RocketEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+        return LayerDefinition.create(meshdefinition, 128, 128);
+    }
 
-	}
+    @Override
+    public void setupAnim(RocketEntity entity,
+                          float limbSwing,
+                          float limbSwingAmount,
+                          float ageInTicks,
+                          float netHeadYaw,
+                          float headPitch) {
+    }
 
-	@Override
-	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-	}
+    @Override
+    public void renderToBuffer(PoseStack poseStack,
+                               VertexConsumer vertexConsumer,
+                               int packedLight,
+                               int packedOverlay,
+                               float red,
+                               float green,
+                               float blue,
+                               float alpha) {
+
+        bb_main.render(
+                poseStack,
+                vertexConsumer,
+                packedLight,
+                packedOverlay,
+                red, green, blue, alpha
+        );
+    }
 }
