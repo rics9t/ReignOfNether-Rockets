2765335d61db4d43b23e0b971a0f51332ca528ff
rics9t <andreygamer190@gmail.com>
2026-04-27 07:51:38 +0500

    Add RocketModel import in ClientSetup

--- src/main/java/com/rics/ronrockets/client/ClientSetup.java
+++ src/main/java/com/rics/ronrockets/client/ClientSetup.java
@@ -5,6 +5,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import com.rics.ronrockets.client.model.RocketModel;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientSetup {
