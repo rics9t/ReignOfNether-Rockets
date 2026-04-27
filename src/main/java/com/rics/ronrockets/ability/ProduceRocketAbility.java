2f3c479688f2c4761bc7e11c24fd0996cba6cb6e
rics9t <andreygamer190@gmail.com>
2026-04-28 00:09:55 +0500

    Refactor ProduceRocketAbility to singleton pattern
    Refactored ProduceRocketAbility to use a singleton instance and removed unused methods and imports.

--- src/main/java/com/rics/ronrockets/ability/ProduceRocketAbility.java
+++ src/main/java/com/rics/ronrockets/ability/ProduceRocketAbility.java
@@ -1,40 +1,17 @@
 package com.rics.ronrockets.ability;
 
 import com.solegendary.reignofnether.ability.Ability;
-import com.solegendary.reignofnether.building.BuildingPlacement;
-import com.solegendary.reignofnether.hud.AbilityButton;
-import com.solegendary.reignofnether.keybinds.Keybinding;
 import com.solegendary.reignofnether.unit.UnitAction;
-
-import net.minecraft.resources.ResourceLocation;
-
-import java.util.List;
 
 public class ProduceRocketAbility extends Ability {
 
     public static final int MAX_ROCKETS = 2;
 
-    public ProduceRocketAbility() {
+    // Single shared instance used as the key for BuildingPlacement charges
+    public static final ProduceRocketAbility INSTANCE = new ProduceRocketAbility();
+
+    private ProduceRocketAbility() {
         super(UnitAction.NONE, 0, 0, 0, false);
         this.maxCharges = MAX_ROCKETS;
-    }
-
-    // ✅ CRITICAL FIX:
-    // Return a hidden dummy button so Abilities system does not insert null
-    @Override
-    public AbilityButton getButton(Keybinding hotkey, BuildingPlacement placement) {
-        return new AbilityButton(
-                "",
-                ResourceLocation.fromNamespaceAndPath("minecraft", "textures/item/barrier.png"),
-                hotkey,
-                () -> false,
-                () -> true,
-                () -> false,
-                () -> {},
-                null,
-                List.of(),
-                this,
-                placement
-        );
     }
 }
