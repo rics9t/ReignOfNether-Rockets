d2c5f0a4a0404075f68f6ca2d2d767bc12f3e7c7
rics9t <andreygamer190@gmail.com>
2026-04-27 22:27:39 +0500

    Enhance AbstractRocketSilo with keybindings
    Added keybinding for LaunchRocketAbility and reorganized imports.

--- src/main/java/com/rics/ronrockets/building/AbstractRocketSilo.java
+++ src/main/java/com/rics/ronrockets/building/AbstractRocketSilo.java
@@ -6,20 +6,17 @@ import com.solegendary.reignofnether.building.production.ProductionBuilding;
 import com.solegendary.reignofnether.keybinds.Keybindings;
 import com.solegendary.reignofnether.resources.ResourceCost;
 
-// ✅ Extended ProductionBuilding so it gets the native Queue UI!
 public abstract class AbstractRocketSilo extends ProductionBuilding {
 
     public AbstractRocketSilo(String structureName) {
         super(structureName, ResourceCost.Building(1000, 800, 600, 0), false);
         this.name = "Rocket Silo";
 
-        this.abilities.add(new LaunchRocketAbility());
-        
-        // ✅ Hooks your custom Rocket Item into Reign of Nether's build queue
+        // hotkeys needed
         this.productions.add(RocketProduction.ROCKET_PROD, Keybindings.keyQ);
+        this.abilities.add(new LaunchRocketAbility(), Keybindings.keyW);
     }
 
-    // ✅ Multiplies all damage by 4, drastically reducing its effective ~300 block health down to ~75 equivalent health
     @Override
     public float getMeleeDamageMult() { return 0.6f; }
 }
