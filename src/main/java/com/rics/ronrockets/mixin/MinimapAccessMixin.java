3e9421b5006434a0b41f2f243bdf4faaff36954d
Rics9t <andreygamer190@gmail.com>
2026-05-02 17:26:35 +0500

    Compil errors

--- src/main/java/com/rics/ronrockets/mixin/MinimapAccessMixin.java
+++ src/main/java/com/rics/ronrockets/mixin/MinimapAccessMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(MinimapClientEvents.class)
 public interface MinimapAccessMixin {
 
-    @Invoker("getWorldPosOnMinimap")
+    @Invoker(value = "getWorldPosOnMinimap", remap = false)
     static BlockPos ronrockets$getWorldPosOnMinimap(float x, float y, boolean offsetForCamera) {
         throw new AssertionError();
     }
